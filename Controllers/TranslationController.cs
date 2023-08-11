7d5b18b7c5ece85a66c1aee59c8d3fb22b722f12
Thevindu Kulasinghe <kulasinghet@gmail.com>
2023-08-11 17:38:16 +0530

    Change MP3 to WebM_Opus

--- Controllers/TranslationController.cs
+++ Controllers/TranslationController.cs
@@ -9,6 +9,9 @@ using Newtonsoft.Json.Linq;
 using NAudio.Wave;
 using static System.Net.WebRequestMethods;
 using System.Net.Http;
+using Microsoft.SqlServer.Server;
+
+using NAudio;
 
 namespace Be_My_Voice_Backend.Controllers
 {
@@ -211,9 +214,6 @@ namespace Be_My_Voice_Backend.Controllers
                     return new APIResponse(406, false, "Please provide a valid audio file in base64 format", base64Audio);
                 }
 
-
-
-
                 int calculatedSampleRate = GetSampleRateFromMemoryStream(base64Audio);
 
                 var requestBody = new
@@ -297,6 +297,56 @@ namespace Be_My_Voice_Backend.Controllers
             }
         }
 
+        [HttpPost("webm-to-mp3")]
+        public async Task<ActionResult<APIResponse>> TranslateWebMtoMp3([FromBody] string base64Webm)
+        {
+            try
+            {
+                // Convert base64 string to byte array
+                byte[] webmBytes = Convert.FromBase64String(base64Webm);
+
+                // Create a temporary file path for the input WebM file
+                string webmFilePath = Path.GetTempFileName() + ".webm";
+
+                // Write the base64 data to the temporary file
+                await System.IO.File.WriteAllBytesAsync(webmFilePath, webmBytes);
+
+                // Create a temporary file path for the output MP3 file
+                string mp3FilePath = Path.GetTempFileName() + ".mp3";
+
+                // Convert the WebM to MP3
+                var converter = new FFMpegConverter();
+                converter.ConvertMedia(webmFilePath, mp3FilePath, Format.mp3);
+
+                // Delete the temporary WebM file
+                System.IO.File.Delete(webmFilePath);
+
+                // Read the MP3 file as byte array
+                byte[] mp3Bytes = await System.IO.File.ReadAllBytesAsync(mp3FilePath);
+
+                // Delete the temporary MP3 file
+                System.IO.File.Delete(mp3FilePath);
+
+                // Convert the MP3 byte array to base64 string
+                string base64Mp3 = Convert.ToBase64String(mp3Bytes);
+
+                Object[] objects = { base64Mp3 };
+
+                return new APIResponse
+                {
+                    Code = 200,
+                    Success = true,
+                    Message = "MP3 Base64",
+                    Data = objects
+                }; // You'll need to define the APIResponse class
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new APIResponse { Message = ex.Message });
+            }
+
+        }
 
         private string ParseResponseAndGetTranslatedText(string responseContent)
         {
