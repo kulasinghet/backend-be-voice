c7d38fcc9ff06706bbddfb5a923c17acac21b3c6
Thevindu Kulasinghe <kulasinghet@gmail.com>
2023-08-11 10:46:47 +0530

    Allow CORS policy

--- Program.cs
+++ Program.cs
@@ -7,12 +7,30 @@ using Serilog.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var allowedOrigins = "_allowedOrigins";
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(
+
+    options =>
+    {
+        options.AddPolicy(
+            allowedOrigins,
+            // allow all origins
+            policy =>
+                {
+                    policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
+                }
+            );
+    }
+    
+);
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
@@ -27,7 +45,7 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
 
 // Dependency Injection
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<ISessionsRepository, SessionsRepository>();  
+builder.Services.AddScoped<ISessionsRepository, SessionsRepository>();
 builder.Services.AddScoped<ITranslationsRepository, TranslationsRepository>();
 
 var app = builder.Build();
@@ -35,11 +53,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 //}
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors(allowedOrigins);
 
 app.UseAuthorization();
 
